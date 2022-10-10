d2f8241a418b8eb6d1d723cba53808a2c2734d3c
ArtemKlym <artemklymenko892@gmail.com>
2022-10-10 17:07:07 +0300

    Add Multidimensional Array

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -7,21 +7,38 @@ public class Main {
         ArrayList<Float> floatList = new ArrayList<>();
 
         float[] floatArr = new float[5];
-        float x = 1;
-
+        double[][] multArray = {
+                {1.0, 0.9, 0.8},
+                {0.7, 0.6, 0.5, 0.4},
+                {0.12},
+        };
+        float a = 1;
 
         for(int i = 0;i<floatArr.length;i++){
-            floatArr[i] = x;
-            floatList.add(x);
-            x = x/2;
+            floatArr[i] = a;
+            floatList.add(a);
+            a = a / 2;
         }
 
-        System.out.println("Float array");
+        System.out.println("\nFloat array");
         for (float num:floatArr) {
             System.out.print(num+", ");
         }
 
-        System.out.println("\nFloat ArrayList");
+        System.out.println("\n\nFloat ArrayList");
         System.out.println(floatList);
+
+        System.out.println("\nMultidimensional Array");
+        for (int i = 0; i < multArray.length; ++i) {
+            for(int j = 0; j < multArray[i].length; ++j) {
+                for (int k = 0; k <= j; k++) {
+                    System.out.println("intArray [" + i
+                            + "][" + j + "][" +k +"] = " + multArray[i][j]);
+                }
+            }
+
+        }
+
+
     }
 }
